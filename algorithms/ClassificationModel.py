3e00fd08ee97e860cf0656cc358cee2f12abdc96
root <root@e2e-71-236.e2enetworks.net.in>
2020-11-08 13:00:48 +0530

    Adding the newer feaure

--- algorithms/ClassificationModel.py
+++ algorithms/ClassificationModel.py
@@ -16,6 +16,10 @@ import time
 from . import Algorithm
 from pdb import set_trace as breakpoint
 
+#from random import seed
+#from random import randint
+# seed random number generator
+np.random.seed(42)
 
 def accuracy(output, target, topk=(1,)):
     """Computes the precision@k for the specified values of k"""
@@ -40,6 +44,7 @@ class ClassificationModel(Algorithm):
         self.tensors = {}
         self.tensors['dataX'] = torch.FloatTensor()
         self.tensors['labels'] = torch.LongTensor()
+        self.tensors['labels1'] = torch.LongTensor()
 
     def train_step(self, batch):
         return self.process_batch(batch, do_train=True)
@@ -52,9 +57,13 @@ class ClassificationModel(Algorithm):
         start = time.time()
         self.tensors['dataX'].resize_(batch[0].size()).copy_(batch[0])
         self.tensors['labels'].resize_(batch[1].size()).copy_(batch[1])
+        self.tensors['labels1'].resize_(batch[2].size()).copy_(batch[2])
         dataX = self.tensors['dataX']
         labels = self.tensors['labels']
+        labels1 = self.tensors['labels1']
         batch_load_time = time.time() - start
+        #print("Labels:", labels)
+        #print("HLabels:", labels1)
         #********************************************************
 
         #********************************************************
@@ -66,15 +75,28 @@ class ClassificationModel(Algorithm):
         #***************** SET TORCH VARIABLES ******************
         dataX_var = torch.autograd.Variable(dataX, volatile=(not do_train))
         labels_var = torch.autograd.Variable(labels, requires_grad=False)
+        labels_var1 = torch.autograd.Variable(labels1, requires_grad=False)
+        #print("Getting called Label size:",labels_var.size())
         #********************************************************
 
         #************ FORWARD THROUGH NET ***********************
-        pred_var = self.networks['model'](dataX_var)
+        fc_feat, pred_var, pred_var1 = self.networks['model'](dataX_var)
+        #pred_var = self.networks['model'](dataX_var)
+        #print("Shape of predictions:",pred_var.size())
         #********************************************************
-
+        
         #*************** COMPUTE LOSSES *************************
         record = {}
-        loss_total = self.criterions['loss'](pred_var, labels_var)
+        loss_mse = self.criterions['mseloss'](fc_feat[0::16,:],fc_feat[np.random.randint(0,16)::16,:])
+        #loss_mse = self.criterions['mseloss'](fc_feat[0::16,:],fc_feat[np.random.randint(0,16)::16,:])
+        loss_cross_entropy1 = self.criterions['loss'](pred_var, labels_var) 
+        loss_cross_entropy2 = self.criterions['loss'](pred_var1,labels_var1)
+        print("Loss cross entroy 1:", loss_cross_entropy1.data, "cross entropy2:",loss_cross_entropy2.data, "MSE:", loss_mse.data)
+        loss_total = loss_cross_entropy1 + loss_cross_entropy2 + loss_mse
+        #loss_total = 0.05*loss_cross_entropy1 + 0.05*loss_cross_entropy2 + 0.9*loss_mse
+        #loss_total = self.criterions['loss'](pred_var, labels_var) + self.criterions['loss'](pred_var1,labels_var1) +self.criterions['mseloss'](fc_feat[0::16,:],fc_feat[np.random.randint(0,16)::16,:])
+        #loss_total = self.criterions['loss'](pred_var, labels_var) 
+        #print("Prediction shape:", pred_var.size())
         record['prec1'] = accuracy(pred_var.data, labels, topk=(1,))[0]#[0]
         record['loss'] = loss_total.data#[0]
         #********************************************************
@@ -88,5 +110,4 @@ class ClassificationModel(Algorithm):
         total_time = batch_process_time + batch_load_time
         record['load_time'] = 100*(batch_load_time/total_time)
         record['process_time'] = 100*(batch_process_time/total_time)
-
         return record
--- architectures/AlexNet.py
+++ architectures/AlexNet.py
@@ -9,18 +9,15 @@ from pdb import set_trace as breakpoint
 class Flatten(nn.Module):
     def __init__(self):
         super(Flatten, self).__init__()
-
     def forward(self, feat):
         return feat.view(feat.size(0), -1)
-
 
 class AlexNet(nn.Module):
     def __init__(self, opt):
         super(AlexNet, self).__init__()
         num_classes = opt['num_classes']
-
         conv1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=14, stride=1, padding=2), # stride = 4
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2), # stride = 1, kernel_size = 14 for 64x64
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
         )
@@ -48,7 +45,6 @@ class AlexNet(nn.Module):
         )
         pool5 = nn.MaxPool2d(kernel_size=3, stride=2)
         #pool5 = nn.MaxPool2d(kernel_size=1, stride=1)
-
         num_pool5_feats = 6 * 6 * 256
         fc_block = nn.Sequential(
             Flatten(),
@@ -62,7 +58,9 @@ class AlexNet(nn.Module):
         classifier = nn.Sequential(
             nn.Linear(4096, num_classes),
         )
-
+        classifier_p = nn.Sequential(
+            nn.Linear(4096,num_classes),
+        )
         self._feature_blocks = nn.ModuleList([
             conv1,
             pool1,
@@ -74,6 +72,7 @@ class AlexNet(nn.Module):
             pool5,
             fc_block,
             classifier,
+            classifier_p,
         ])
         self.all_feat_names = [
             'conv1',
@@ -86,40 +85,38 @@ class AlexNet(nn.Module):
             'pool5',
             'fc_block',
             'classifier',
+            'classifier_p',
         ]
         assert(len(self.all_feat_names) == len(self._feature_blocks))
 
     def _parse_out_keys_arg(self, out_feat_keys):
-
         # By default return the features of the last layer / module.
-        out_feat_keys = [self.all_feat_names[-1],
+        out_feat_keys = [self.all_feat_names[-3:],
                          ] if out_feat_keys is None else out_feat_keys
-
+        out_feat_keys = out_feat_keys[0]
+        #print("Out_feat_keys:", out_feat_keys)
         if len(out_feat_keys) == 0:
             raise ValueError('Empty list of output feature keys.')
         for f, key in enumerate(out_feat_keys):
+            #print("Iteration:,", f, key)
             if key not in self.all_feat_names:
                 raise ValueError('Feature with name {0} does not exist. Existing features: {1}.'.format(
                     key, self.all_feat_names))
             elif key in out_feat_keys[:f]:
                 raise ValueError(
                     'Duplicate output feature key: {0}.'.format(key))
-
         # Find the highest output feature in `out_feat_keys
         max_out_feat = max([self.all_feat_names.index(key)
                             for key in out_feat_keys])
-
         return out_feat_keys, max_out_feat
 
     def forward(self, x, out_feat_keys=None):
         """Forward an image `x` through the network and return the asked output features.
-
         Args:
           x: input image.
           out_feat_keys: a list/tuple with the feature names of the features
                 that the function should return. By default the last feature of
-                the network is returned.
-
+                the network is returned.\
         Return:
             out_feats: If multiple output features were asked then `out_feats`
                 is a list with the asked output features placed in the same
@@ -128,17 +125,32 @@ class AlexNet(nn.Module):
         """
         out_feat_keys, max_out_feat = self._parse_out_keys_arg(out_feat_keys)
         out_feats = [None] * len(out_feat_keys)
-
+        #print("Outfeat keys:", out_feat_keys)
         feat = x
-        for f in range(max_out_feat+1):
-            feat = self._feature_blocks[f](feat)
-            key = self.all_feat_names[f]
-            #print('key:',key)
-            #print('feat:',feat.size())
-            if key in out_feat_keys:
-                out_feats[out_feat_keys.index(key)] = feat
-
+        for f in range(max_out_feat):
+            if f>=(max_out_feat-1):
+                feat1 = self._feature_blocks[f](feat)
+                key1 = self.all_feat_names[f]
+                if key1 in out_feat_keys:
+                    out_feats[out_feat_keys.index(key1)] = feat1
+                feat2 = self._feature_blocks[f+1](feat)
+                key2 = self.all_feat_names[f+1]
+                if key2 in out_feat_keys:
+                    out_feats[out_feat_keys.index(key2)] = feat2
+                #print('key:',key1)
+                #print('feat:',feat1.size())
+                #print('key:',key2)
+                #print('feat:',feat2.size())
+            else:
+                feat = self._feature_blocks[f](feat)
+                key = self.all_feat_names[f]
+                #print('key:',key)
+                #print('feat:',feat.size())
+                if key in out_feat_keys:
+                    out_feats[out_feat_keys.index(key)] = feat
+        #print("len of out_feats:", len(out_feats))
         out_feats = out_feats[0] if len(out_feats) == 1 else out_feats
+        #print("Forward pass output size:",len(out_feats))
         return out_feats
 
     def get_L1filters(self):
